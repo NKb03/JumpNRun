18c71e78a3bacbaf8dd957bd8fbb43dcef690e8c
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-24 10:01:50 +0100

    Create Line.intersects(Circle) function

--- app/src/main/java/org/nikok/jumpnrun/graphic/Geometry.kt
+++ app/src/main/java/org/nikok/jumpnrun/graphic/Geometry.kt
@@ -2,6 +2,7 @@ package org.nikok.jumpnrun.graphic
 
 import kotlin.math.pow
 
+
 fun overlaps(circle: Circle, rect: Rect): Boolean {
     val halfWidth = rect.width / 2
     val cx = Math.abs(circle.cx - rect.x - halfWidth)
@@ -31,3 +32,29 @@ fun overlaps(x1: Float, y1: Float, x2: Float, y2: Float, r1: Float, r2: Float): 
     val radii = r2 + r1
     return dx.pow(2) + dy.pow(2) < radii.pow(2)
 }
+
+typealias Line = Pair<Point, Point>
+
+fun Line.intersects(circle: Circle): Boolean {
+    val deltaX = second.x - first.x
+    val deltaY = second.y - first.y
+    val a = deltaX * deltaX + deltaY * deltaY
+    val b = 2 * (deltaX * (first.x - circle.cx) + deltaY * (first.y - circle.cy))
+    val c =
+        (first.x - circle.cx) * (first.x - circle.cx) + (first.y - circle.cy) * (first.y - circle.cy) - circle.radius * circle.radius
+    val discriminant = b * b - 4.0 * a * c
+    if (discriminant < 0)
+        return false
+    val quad1 = (-b + Math.sqrt(discriminant)) / (2 * a)
+    val quad2 = (-b - Math.sqrt(discriminant)) / (2 * a)
+    if (quad1 in 0.0..1.0)
+        return true
+    else if (quad2 in 0.0..1.0)
+        return true
+    return false
+}
+
+val Rect.upperLeft get() = Point(x, y)
+val Rect.upperRight get() = Point(x + width, y)
+
+val Rect.upperSide get() = upperLeft to upperRight
